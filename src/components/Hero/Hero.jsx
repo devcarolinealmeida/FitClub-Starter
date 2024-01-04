67c9d1a7591cebf413301ab27b05ae05c819ca10
devcarolinealmeida <carolineitalia85@gmail.com>
2024-01-04 15:18:05 +0100

    add hero-text and figures inside Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,9 +8,38 @@ const Hero = () => {
     <div className='hero'>
         <div className='left-h'>
             <Header/>
+            {/* the best ad */}
             <div className="the-best-ad">
                 <div></div>
                 <span>the best fitness club in the town</span>
+            </div>
+            {/* Hero Heading */}
+            <div className="hero-text">
+                <div>
+                    <span className='stroke-text'>Shape </span>
+                    <span>Your</span>
+                </div>
+                <div>
+                    <span>Ideal body</span>
+                </div>
+                <div>
+                    <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                </div>
+            </div>
+            {/* Hero data figures */}
+            <div className="figures">
+                <div>
+                    <span>+ 140</span>
+                    <span>Expert coaches</span>
+                </div>
+                <div>
+                    <span>+ 978</span>
+                    <span>Members joined</span>
+                </div>
+                <div>
+                    <span>+ 50</span>
+                    <span>Fitness programs</span>
+                </div>
             </div>
         </div>
         <div className='right-h'>right side</div>
