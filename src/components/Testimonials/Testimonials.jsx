ce8d850dcec597a0eee680f4795dc13f9db53dfb
devcarolinealmeida <carolineitalia85@gmail.com>
2024-01-10 11:43:15 +0100

    add motion transition to images from testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,10 +3,12 @@ import "./Testimonials.css";
 import leftArrow from "../../assets/leftArrow.png";
 import rightArrow from "../../assets/rightArrow.png";
 import { testimonialsData } from "../../data/testimonialsData";
+import { motion } from "framer-motion";
 
 const Testimonials = () => {
-    const [selected, setSelected] = useState(0);
-    const testimonialLength = testimonialsData.length;
+  const transition = { type: 'spring', duration: 3}
+  const [selected, setSelected] = useState(0);
+  const testimonialLength = testimonialsData.length;
 
   return (
     <div className="testimonials">
@@ -24,8 +26,16 @@ const Testimonials = () => {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
+        <motion.div
+        initial={{ opacity: 0, x: -100}}
+        transition={{ ...transition, duration: 2 }}
+        whileInView={{ opacity: 1, x: 0}}
+        ></motion.div>
+        <motion.div
+        initial={{ opacity:0, x: 100}}
+        transition={{...transition, duration: 2}}
+        whileInView={{ opacity:1, x: 0 }}
+        ></motion.div>
         <img src={testimonialsData[selected].image} alt="" />
         <div className="arrows">
             <img 
